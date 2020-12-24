f37de400bd01b2987688a9101bdda8ce997c1fda
cameronclass <kamrankutumov@gmail.com>
2020-12-24 14:02:51 +0300

    initial commit

--- dist/assets/js/common/preloader.js
+++
@@ -1,1 +0,0 @@
-$(document).ready((function(){$("#page-preloader").fadeOut(1e3),AOS.init({once:!0,anchorPlacement:"bottom-bottom",duration:800}),onElementHeightChange(document.body,(function(){AOS.refresh()}))}));
